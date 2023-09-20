ee588bf308828a6f0f4fe03b8e8a5299abed55a2
billal-webdev <billal-webdev@gmail.com>
2023-09-20 16:22:55 +0600

    react bootstrap, firebase setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from 'react-bootstrap';
+function App() {
+  return (
+    <div>
+      <h1>Hello, react</h1>
+      <Button>React Bootstrap</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ_ebwvKlW2lHe_crDmV1dNfgU9q67Lik",
+  authDomain: "firestore-crud-7bfc4.firebaseapp.com",
+  projectId: "firestore-crud-7bfc4",
+  storageBucket: "firestore-crud-7bfc4.appspot.com",
+  messagingSenderId: "926366244010",
+  appId: "1:926366244010:web:3daa5aef20d8364aaa44d3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
